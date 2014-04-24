2859178d5821fdd97a335b268524d8275b212eee
Joe <evonsdesigns@gmail.com>
2014-04-23 22:23:51 -0500

    smallest sequence problem

---
+++ src/careercup/SmallestSequence.java
@@ -0,0 +1,52 @@
+package careercup;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * http://www.careercup.com/question?id=5653018213089280
+ */
+public class SmallestSequence {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        List<Integer> variables = new ArrayList<Integer>();
+        int sum = Integer.parseInt(s.nextLine());
+        while(s.hasNextLine()) {
+            String input = s.nextLine();
+            if(input.equals("exit")) {
+                break;
+            }
+            else {
+                variables.add(Integer.parseInt(input));
+            }
+
+        }
+        /// all the variables are now in the array
+        int counter = 0;
+        int smallestSequence = Integer.MAX_VALUE;
+        for(int i = 0; i <  variables.size(); i++) {
+            counter = 1;
+            int total = variables.get(i);
+            if(total > sum) {
+                smallestSequence = 1;
+                break;
+            }
+            for(int j = i + 1; j < variables.size(); j++) {
+                total += variables.get(j);
+                counter++;
+                if(total > sum) {
+                    if(counter < smallestSequence) {
+                        smallestSequence = counter;
+                    }
+                    break;
+                }
+            }
+
+        }
+        System.out.println(smallestSequence);
+        s.close();
+    }
+
+}
