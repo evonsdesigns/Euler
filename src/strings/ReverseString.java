ac6bdf4752f12d898d745bd7ddfea36b84e68dff
Joe <evonsdesigns@gmail.com>
2014-05-26 14:12:18 -0500

    added optimized version of reverse string
    works well for large strings, while regular works best for small
    strings. this is because the original will take up more memory in the
    long run due to constantly creating new Strings. The optimized version
    only creates it once with the constructor

--- src/strings/ReverseString.java
+++ src/strings/ReverseString.java
@@ -1,5 +1,8 @@
 package strings;
 
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
 /**
  * Created by josep_000 on 5/26/2014.
  */
@@ -8,9 +11,29 @@ public class ReverseString {
     public static String reverse(String input) {
         char[] arr = input.toCharArray();
         String backwards = "";
+        long start = System.nanoTime();
         for(int i = arr.length - 1;i >= 0; i--) {
             backwards += arr[i];
         }
+
+        System.out.println("Reverse Runtime: " + (System.nanoTime() - start) + " ns");
+        return backwards;
+
+
+    }
+
+
+    public static String reverseOptimized(String input) {
+        char[] arr = input.toCharArray();
+
+        long start = System.nanoTime();
+        for(int i = 0, j = arr.length-1; i < arr.length/2; i++, j--) {
+            char c = arr[j];
+            arr[j] = arr[i];
+            arr[i] = c;
+        }
+        String backwards = new String(arr);
+        System.out.println("Reverse Optimized Runtime: " + (System.nanoTime() - start) + " ns");
         return backwards;
     }
 }
--- src/test/strings/ReverseStringTest.java
+++ src/test/strings/ReverseStringTest.java
@@ -10,8 +10,28 @@ import static org.junit.Assert.assertEquals;
  */
 public class ReverseStringTest {
 
+    private String veryLongString = "poKEwg9034j[tk3okpspdkfPO#kflz,mofwiojwklj3row3pga9baemiohjaw9u4390wjag[esIGJK(OSGU(JEG)IMlk;mselkfj;ioWEJF(W#PHt9832u205p2390*%)(@*#%ji23oi4joiJ:Asklsncknzxnfowiehjfuiowhjetiohweiofjawpeijga98w34jtp983jw592up52j3ioajpeofjipoijioejcioJE#ROP*(jtpjwwppioejpoitwjpoiejfiopjzxcmvxnvxm,nvbmlnv,ncvmNCXM<VNCXM,vCX<vn.,mz..Z>Z>Z>Z>W902)!))!)!))!)!($$($";
+    private String veryLongStringReversed = "$($$(!)!))!)!))!)209W>Z>Z>Z>Z..zm,.nv<XCv,MXCNV<MXCNmvcn,vnlmbvn,mxvnxvmcxzjpoifjeiopjwtiopjeoippwwjptj(*POR#EJoicjeoijiopijfoepjaoi3j25pu295wj389ptj43w89agjiepwajfoiewhoitejhwoiufjheiwofnxznkcnslksA:Jioj4io32ij%#*@()%*0932p502u2389tHP#W(FJEWoi;jfklesm;klMI)GEJ(UGSO(KJGIse[gajw0934u9wajhoimeab9agp3wor3jlkwjoiwfom,zlfk#OPfkdpspko3kt[j4309gwEKop";
+
     @Test
     public void testReverseString() {
         assertEquals("dlroW olleH", ReverseString.reverse("Hello World"));
     }
+
+    @Test
+    public void testReverseStringOptimized() {
+        assertEquals("dlroW olleH", ReverseString.reverseOptimized("Hello World"));
+    }
+
+    @Test
+    public void testReverseStringVeryLargeString() {
+        assertEquals(veryLongStringReversed, ReverseString.reverse(veryLongString));
+    }
+
+    @Test
+    public void testReverseStringOptimizedVeryLargeString() {
+        assertEquals(veryLongStringReversed, ReverseString.reverseOptimized(veryLongString));
+    }
+
+
 }
